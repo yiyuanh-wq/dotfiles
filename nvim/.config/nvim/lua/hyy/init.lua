fb42e7d305740edd4c25a9b58de77336516c3a3e
nanigen <yh8953@utexas.edu>
2023-08-28 16:47:14 -0500

    refactor nvim config

--- nvim/.config/nvim/lua/hyy/init.lua
+++ nvim/.config/nvim/lua/hyy/init.lua
@@ -1,3 +1,14 @@
+-- core
 require("hyy.lazy")
-require("hyy.remap")
 require("hyy.set")
+require("hyy.remap")
+
+-- user plugins
+require("hyy.colorscheme")
+require("hyy.comment")
+require("hyy.gitsigns")
+require("hyy.harpoon")
+require("hyy.lsp")
+require("hyy.numb")
+require("hyy.telescope")
+require("hyy.treesitter")
--- nvim/.config/nvim/lua/hyy/remap.lua
+++ nvim/.config/nvim/lua/hyy/remap.lua
@@ -1,29 +1,5 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", ":NeoTreeFocusToggle<CR>")
-vim.keymap.set("n", "<leader>fm", ":lua vim.lsp.buf.format()<CR>")
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("i", "jk", "<ESC>")
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
-
-local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
-
-
-local mark = require("harpoon.mark")
-local ui = require("harpoon.ui")
-vim.keymap.set("n", "<leader>a", mark.add_file)
-vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
-
-vim.keymap.set("n", "<C-h>", function()
-	ui.nav_file(1)
-end)
-vim.keymap.set("n", "<C-t>", function()
-	ui.nav_file(2)
-end)
-vim.keymap.set("n", "<C-n>", function()
-	ui.nav_file(3)
-end)
-vim.keymap.set("n", "<C-s>", function()
-	ui.nav_file(4)
-end)
