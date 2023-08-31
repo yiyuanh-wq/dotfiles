1311700cf9d01fb2a6a2edf098341fc860f12169
nanigen <yh8953@utexas.edu>
2023-08-30 22:57:45 -0500

    trying to break habit of using jk for esc in vim

--- nvim/.config/nvim/lua/hyy/remap.lua
+++ nvim/.config/nvim/lua/hyy/remap.lua
@@ -1,5 +1,5 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set("i", "jk", "<ESC>")
+-- vim.keymap.set("i", "jk", "<esc>")
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
