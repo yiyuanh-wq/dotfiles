a2789d9e807e16a72215947a6c81d4ca29b0220c
yiyuanh <yiyuanh@proton.me>
2023-10-19 10:47:03 -0500

    edit lualine nvim

--- nvim/.config/nvim/lua/hyy/lualine.lua
+++ nvim/.config/nvim/lua/hyy/lualine.lua
@@ -22,13 +22,13 @@ require('lualine').setup {
     lualine_b = {'branch', 'diff', 'diagnostics'},
     lualine_c = {},
     lualine_x = {},
-    lualine_y = {},
-    lualine_z = {'location'}
+    lualine_y = {'filename'},
+    lualine_z = {}
   },
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
-    lualine_c = {'filename'},
+    lualine_c = {},
     lualine_x = {'location'},
     lualine_y = {},
     lualine_z = {}
