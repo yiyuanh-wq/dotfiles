89ad67baf495decddc3d042bb27e1142036f3a65
yiyuanh <yiyuanh@proton.me>
2023-10-21 11:49:56 -0500

    change colors to catppuccin frappe

--- nvim/.config/nvim/lua/hyy/lazy.lua
+++ nvim/.config/nvim/lua/hyy/lazy.lua
@@ -12,6 +12,7 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
   { 'windwp/nvim-autopairs' },
   { "numToStr/Comment.nvim" },
   { "theprimeagen/harpoon" },
