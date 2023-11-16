89e8b64488fd8cc64505dc8e8f1ae245a1aa40ac
yiyuanh <yiyuanh@proton.me>
2023-11-16 11:01:16 -0600

    add lsp for lua

--- nvim/.config/nvim/lua/hyy/lsp.lua
+++ nvim/.config/nvim/lua/hyy/lsp.lua
@@ -1,6 +1,8 @@
 local status, lsp = pcall(require, "lspconfig")
 if (not status) then return end
 
+lsp.lua_ls.setup {}
+
 lsp.ocamllsp.setup({
-    root_dir = lsp.util.root_pattern("*.opam", "esy.json", "package.json", ".git", "dune-project", "dune-workspace", "*.ml"),
+  root_dir = lsp.util.root_pattern("*.opam", "esy.json", "package.json", ".git", "dune-project", "dune-workspace", "*.ml"),
 })
