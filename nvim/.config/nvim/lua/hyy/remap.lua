ef1fef32ce515c90fe2f37491af881c284b7decc
yiyuanh <yiyuanh@proton.me>
2023-11-15 19:21:51 -0600

    remove unused bindings nvim

--- nvim/.config/nvim/lua/hyy/remap.lua
+++ nvim/.config/nvim/lua/hyy/remap.lua
@@ -3,11 +3,6 @@ local opts = { noremap = true, silent = true }
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>e", ":Lex 30<CR>", opts)
 
-vim.keymap.set("n", "<leader>z", ":ZenMode<CR>", opts)
-
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-
 -- Stay in indent mode
 vim.keymap.set("v", "<", "<gv", opts)
 vim.keymap.set("v", ">", ">gv", opts)
@@ -24,11 +19,6 @@ vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
-
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
