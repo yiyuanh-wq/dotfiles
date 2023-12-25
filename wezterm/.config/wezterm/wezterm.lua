00d34537a76fdf8eb86c4a41f3eb64ab014488d3
yiyuanh <yiyuanh@proton.me>
2023-12-25 10:28:59 -0600

    wezterm modus theme

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -14,7 +14,7 @@ config.audible_bell = "Disabled"
 config.font = wezterm.font 'Liga SFMono Nerd Font'
 config.font_size = 22
 
-config.color_scheme = 'paper'
+config.color_scheme = 'Modus-Operandi-Tinted'
 config.use_fancy_tab_bar = false
 config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
