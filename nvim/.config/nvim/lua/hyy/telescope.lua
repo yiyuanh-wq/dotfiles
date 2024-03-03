fa8609b5ab26e4fd1a69f6237b028bc2b51b7d23
yiyuanh <yiyuanh@proton.me>
2024-03-03 12:32:34 -0800

    change telescope keybinds to use 's'

--- nvim/.config/nvim/lua/hyy/telescope.lua
+++ nvim/.config/nvim/lua/hyy/telescope.lua
@@ -1,4 +1,4 @@
 local opts = { noremap = true, silent = true }
 local keymap = vim.api.nvim_set_keymap
-keymap("n", "<leader>pf", "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = false }))<cr>", opts)
-keymap("n", "<leader>pg", "<cmd>Telescope live_grep<cr>", opts)
+keymap("n", "<leader>sf", "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = false }))<cr>", opts)
+keymap("n", "<leader>sg", "<cmd>Telescope live_grep<cr>", opts)
