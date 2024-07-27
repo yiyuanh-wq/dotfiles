641c9ebb9dfc117304e32b55a6d093b3817601bc
yiyuanh <yiyuanh@proton.me>
2024-07-26 22:10:14 -0700

    feat: add tmux bindings to wezterm

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -13,7 +13,7 @@ if wezterm.config_builder then
 end
 
 config.audible_bell = "Disabled"
-config.font = wezterm.font "PragmataPro Mono"
+config.font = wezterm.font "JetBrains Mono"
 config.font_size = 18
 
 config.color_scheme = "Kanagawa (Gogh)"
@@ -24,8 +24,39 @@ config.tab_bar_at_bottom = true
 config.window_decorations = "RESIZE"
 config.force_reverse_video_cursor = true
 config.debug_key_events = true
+config.hide_tab_bar_if_only_one_tab = true
+
+config.leader = { key="a", mods="CTRL", timeout_milliseconds = 1000 }
 
 config.keys = {
+  { key = "a", mods = "LEADER|CTRL",  action=wezterm.action{SendString="\x01"}},
+  { key = "-", mods = "LEADER",       action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}}},
+  { key = "\\",mods = "LEADER",       action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}}},
+  { key = "s", mods = "LEADER",       action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}}},
+  { key = "v", mods = "LEADER",       action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}}},
+  { key = "o", mods = "LEADER",       action="TogglePaneZoomState" },
+  { key = "z", mods = "LEADER",       action="TogglePaneZoomState" },
+  { key = "c", mods = "LEADER",       action=wezterm.action{SpawnTab="CurrentPaneDomain"}},
+  { key = "h", mods = "LEADER",       action=wezterm.action{ActivatePaneDirection="Left"}},
+  { key = "j", mods = "LEADER",       action=wezterm.action{ActivatePaneDirection="Down"}},
+  { key = "k", mods = "LEADER",       action=wezterm.action{ActivatePaneDirection="Up"}},
+  { key = "l", mods = "LEADER",       action=wezterm.action{ActivatePaneDirection="Right"}},
+  { key = "H", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Left", 5}}},
+  { key = "J", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Down", 5}}},
+  { key = "K", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Up", 5}}},
+  { key = "L", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Right", 5}}},
+  { key = "1", mods = "LEADER",       action=wezterm.action{ActivateTab=0}},
+  { key = "2", mods = "LEADER",       action=wezterm.action{ActivateTab=1}},
+  { key = "3", mods = "LEADER",       action=wezterm.action{ActivateTab=2}},
+  { key = "4", mods = "LEADER",       action=wezterm.action{ActivateTab=3}},
+  { key = "5", mods = "LEADER",       action=wezterm.action{ActivateTab=4}},
+  { key = "6", mods = "LEADER",       action=wezterm.action{ActivateTab=5}},
+  { key = "7", mods = "LEADER",       action=wezterm.action{ActivateTab=6}},
+  { key = "8", mods = "LEADER",       action=wezterm.action{ActivateTab=7}},
+  { key = "9", mods = "LEADER",       action=wezterm.action{ActivateTab=8}},
+  { key = "&", mods = "LEADER|SHIFT", action=wezterm.action{CloseCurrentTab={confirm=true}}},
+  { key = "d", mods = "LEADER",       action=wezterm.action{CloseCurrentPane={confirm=true}}},
+  { key = "x", mods = "LEADER",       action=wezterm.action{CloseCurrentPane={confirm=true}}},
   {
     key = 'c',
     mods = 'CMD',
@@ -35,42 +66,6 @@ config.keys = {
     key = 'v',
     mods = 'CMD',
     action = wezterm.action.PasteFrom 'Clipboard',
-  },
-  {
-    key = '"',
-    mods = 'CTRL|SHIFT',
-    action = wezterm.action.SplitPane {
-      direction = 'Left',
-      size = { Percent = 50 }
-    }
-  },
-  {
-    key = '%',
-    mods = 'CTRL|SHIFT',
-    action = wezterm.action.SplitPane {
-      direction = 'Down',
-      size = { Percent = 50 }
-    }
-  },
-  {
-    key = 'h',
-    mods = 'CTRL|SHIFT',
-    action = wezterm.action.ActivatePaneDirection 'Left',
-  },
-  {
-    key = 'l',
-    mods = 'CTRL|SHIFT',
-    action = wezterm.action.ActivatePaneDirection 'Right',
-  },
-  {
-    key = 'k',
-    mods = 'CTRL|SHIFT',
-    action = wezterm.action.ActivatePaneDirection 'Up',
-  },
-  {
-    key = 'j',
-    mods = 'CTRL|SHIFT',
-    action = wezterm.action.ActivatePaneDirection 'Down',
   },
   -- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
   {
