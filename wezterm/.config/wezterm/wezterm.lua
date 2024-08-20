699832325158790ec264632a37d0caca05eceb4c
yiyuanh <yiyuanh@proton.me>
2024-08-19 17:18:21 -0700

    chore: Cleanup and Reformat Wezterm Config

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -16,7 +16,7 @@ config.audible_bell = "Disabled"
 config.font = wezterm.font("JetBrains Mono")
 config.font_size = 18
 
-config.color_scheme = "Tokyo Night Day"
+config.color_scheme = "Tokyo Night"
 
 config.use_fancy_tab_bar = false
 config.enable_tab_bar = true
@@ -26,51 +26,138 @@ config.force_reverse_video_cursor = true
 config.debug_key_events = true
 config.hide_tab_bar_if_only_one_tab = true
 
-config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
+config.leader = {
+	key = "a",
+	mods = "CTRL",
+	timeout_milliseconds = 1000,
+}
 
 config.keys = {
-	{ key = "a", mods = "LEADER|CTRL", action = wezterm.action({ SendString = "\x01" }) },
-	{ key = "-", mods = "LEADER", action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }) },
+	{
+		key = "a",
+		mods = "LEADER|CTRL",
+		action = wezterm.action({ SendString = "\x01" }),
+	},
+	{
+		key = "-",
+		mods = "LEADER",
+		action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }),
+	},
 	{
 		key = "\\",
 		mods = "LEADER",
 		action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }),
 	},
-	{ key = "s", mods = "LEADER", action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }) },
+	{
+		key = "s",
+		mods = "LEADER",
+		action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }),
+	},
 	{
 		key = "v",
 		mods = "LEADER",
 		action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }),
 	},
-	{ key = "%", mods = "LEADER", action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }) },
+	{
+		key = "%",
+		mods = "LEADER",
+		action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }),
+	},
 	{
 		key = '"',
 		mods = "LEADER",
 		action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }),
 	},
-	{ key = "o", mods = "LEADER", action = "TogglePaneZoomState" },
-	{ key = "z", mods = "LEADER", action = "TogglePaneZoomState" },
-	{ key = "c", mods = "LEADER", action = wezterm.action({ SpawnTab = "CurrentPaneDomain" }) },
-	{ key = "h", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Left" }) },
-	{ key = "j", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Down" }) },
-	{ key = "k", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Up" }) },
-	{ key = "l", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Right" }) },
-	{ key = "H", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Left", 5 } }) },
-	{ key = "J", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Down", 5 } }) },
-	{ key = "K", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Up", 5 } }) },
-	{ key = "L", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Right", 5 } }) },
-	{ key = "1", mods = "LEADER", action = wezterm.action({ ActivateTab = 0 }) },
-	{ key = "2", mods = "LEADER", action = wezterm.action({ ActivateTab = 1 }) },
-	{ key = "3", mods = "LEADER", action = wezterm.action({ ActivateTab = 2 }) },
-	{ key = "4", mods = "LEADER", action = wezterm.action({ ActivateTab = 3 }) },
-	{ key = "5", mods = "LEADER", action = wezterm.action({ ActivateTab = 4 }) },
-	{ key = "6", mods = "LEADER", action = wezterm.action({ ActivateTab = 5 }) },
-	{ key = "7", mods = "LEADER", action = wezterm.action({ ActivateTab = 6 }) },
-	{ key = "8", mods = "LEADER", action = wezterm.action({ ActivateTab = 7 }) },
-	{ key = "9", mods = "LEADER", action = wezterm.action({ ActivateTab = 8 }) },
-	{ key = "&", mods = "LEADER|SHIFT", action = wezterm.action({ CloseCurrentTab = { confirm = true } }) },
-	{ key = "d", mods = "LEADER", action = wezterm.action({ CloseCurrentPane = { confirm = true } }) },
-	{ key = "x", mods = "LEADER", action = wezterm.action({ CloseCurrentPane = { confirm = true } }) },
+	{
+		key = "o",
+		mods = "LEADER",
+		action = "TogglePaneZoomState",
+	},
+	{
+		key = "c",
+		mods = "LEADER",
+		action = wezterm.action({ SpawnTab = "CurrentPaneDomain" }),
+	},
+	{
+		key = "h",
+		mods = "LEADER",
+		action = wezterm.action({ ActivatePaneDirection = "Left" }),
+	},
+	{
+		key = "j",
+		mods = "LEADER",
+		action = wezterm.action({ ActivatePaneDirection = "Down" }),
+	},
+	{
+		key = "k",
+		mods = "LEADER",
+		action = wezterm.action({ ActivatePaneDirection = "Up" }),
+	},
+	{
+		key = "l",
+		mods = "LEADER",
+		action = wezterm.action({ ActivatePaneDirection = "Right" }),
+	},
+	{
+		key = "1",
+		mods = "LEADER",
+		action = wezterm.action({ ActivateTab = 0 }),
+	},
+	{
+		key = "2",
+		mods = "LEADER",
+		action = wezterm.action({ ActivateTab = 1 }),
+	},
+	{
+		key = "3",
+		mods = "LEADER",
+		action = wezterm.action({ ActivateTab = 2 }),
+	},
+	{
+		key = "4",
+		mods = "LEADER",
+		action = wezterm.action({ ActivateTab = 3 }),
+	},
+	{
+		key = "5",
+		mods = "LEADER",
+		action = wezterm.action({ ActivateTab = 4 }),
+	},
+	{
+		key = "6",
+		mods = "LEADER",
+		action = wezterm.action({ ActivateTab = 5 }),
+	},
+	{
+		key = "7",
+		mods = "LEADER",
+		action = wezterm.action({ ActivateTab = 6 }),
+	},
+	{
+		key = "8",
+		mods = "LEADER",
+		action = wezterm.action({ ActivateTab = 7 }),
+	},
+	{
+		key = "9",
+		mods = "LEADER",
+		action = wezterm.action({ ActivateTab = 8 }),
+	},
+	{
+		key = "&",
+		mods = "LEADER|SHIFT",
+		action = wezterm.action({ CloseCurrentTab = { confirm = true } }),
+	},
+	{
+		key = "d",
+		mods = "LEADER",
+		action = wezterm.action({ CloseCurrentPane = { confirm = true } }),
+	},
+	{
+		key = "x",
+		mods = "LEADER",
+		action = wezterm.action({ CloseCurrentPane = { confirm = true } }),
+	},
 	{
 		key = "c",
 		mods = "CMD",
@@ -81,28 +168,31 @@ config.keys = {
 		mods = "CMD",
 		action = wezterm.action.PasteFrom("Clipboard"),
 	},
-	-- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
 	{
-		key = "LeftArrow",
-		mods = "OPT",
-		action = wezterm.action({ SendString = "\x1bb" }),
-	},
-	-- Make Option-Right equivalent to Alt-f; forward-word
-	{
-		key = "RightArrow",
-		mods = "OPT",
-		action = wezterm.action({ SendString = "\x1bf" }),
+		key = "f",
+		mods = "LEADER",
+		action = wezterm.action_callback(sessionizer.toggle),
 	},
 	{
-		key = "l",
-		mods = "ALT",
-		action = wezterm.action.ShowLauncherArgs({ flags = "WORKSPACES" }),
+		key = "=",
+		mods = "CMD",
+		action = wezterm.action.DisableDefaultAssignment,
 	},
-	{ key = "f", mods = "LEADER", action = wezterm.action_callback(sessionizer.toggle) },
-	{ key = "=", mods = "CMD", action = wezterm.action.DisableDefaultAssignment },
-	{ key = "-", mods = "CMD", action = wezterm.action.DisableDefaultAssignment },
-	{ key = "=", mods = "CTRL", action = wezterm.action.DisableDefaultAssignment },
-	{ key = "-", mods = "CTRL", action = wezterm.action.DisableDefaultAssignment },
+	{
+		key = "-",
+		mods = "CMD",
+		action = wezterm.action.DisableDefaultAssignment,
+	},
+	{
+		key = "=",
+		mods = "CTRL",
+		action = wezterm.action.DisableDefaultAssignment,
+	},
+	{
+		key = "-",
+		mods = "CTRL",
+		action = wezterm.action.DisableDefaultAssignment,
+	},
 }
 
 function recompute_padding(window)
