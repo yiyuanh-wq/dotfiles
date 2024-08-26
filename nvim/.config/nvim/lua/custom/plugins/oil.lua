fd022039354eb2a857cf85cef019bad005e34b09
yiyuanh <yiyuanh@proton.me>
2024-08-25 20:09:10 -0700

    feat: modularize nvim config from kickstarter

---
+++ nvim/.config/nvim/lua/custom/plugins/oil.lua
@@ -0,0 +1,9 @@
+return {
+  'stevearc/oil.nvim',
+  opts = {},
+  -- Optional dependencies
+  dependencies = { 'nvim-tree/nvim-web-devicons' }, -- use if prefer nvim-web-devicons
+  config = function()
+    -- write plugin config here
+  end,
+}
