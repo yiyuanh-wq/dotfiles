77453e8c9ac005089d00fa04b7a54051de08b1bb
yiyuanh <yiyuanh@proton.me>
2024-09-01 21:13:22 -0700

    feat: change vim slime to use wezterm as target

--- nvim/.config/nvim/lua/custom/plugins/slime.lua
+++ nvim/.config/nvim/lua/custom/plugins/slime.lua
@@ -1,1 +1,7 @@
-return { 'jpalardy/vim-slime' }
+return {
+  'jpalardy/vim-slime',
+  init = function()
+    vim.cmd [[let g:slime_target = "wezterm"]]
+    vim.cmd [[let g:slime_default_config = {"pane_direction": "right"}]]
+  end,
+}
