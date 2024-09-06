ef863c0d5070296d70d12b5ae97e9e6352f7af1f
Yiyuan He <yiyuanh@amazon.com>
2024-09-06 21:13:36 +0000

    fix: initialize oil plugin

--- nvim/.config/nvim/lua/custom/plugins/oil.lua
+++ nvim/.config/nvim/lua/custom/plugins/oil.lua
@@ -5,5 +5,7 @@ return {
   dependencies = { 'nvim-tree/nvim-web-devicons' }, -- use if prefer nvim-web-devicons
   config = function()
     -- write plugin config here
+    require('oil').setup()
+    vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
   end,
 }
