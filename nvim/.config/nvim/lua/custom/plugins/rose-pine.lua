3d39d945ac1762643744fb786d745a66594824c6
yiyuanh-x1carbon <yiyuanh@proton.me>
2024-09-09 21:31:35 -0700

    feat: add more themes and switch theme to oxocarbon

--- nvim/.config/nvim/lua/custom/plugins/rose-pine.lua
+++ nvim/.config/nvim/lua/custom/plugins/rose-pine.lua
@@ -3,7 +3,7 @@ return {
     "rose-pine/neovim", name = "rose-pine",
     priority = 1000,
     init = function()
-      vim.cmd.colorscheme 'rose-pine'
+      -- vim.cmd.colorscheme 'rose-pine'
     end,
   }
 }
