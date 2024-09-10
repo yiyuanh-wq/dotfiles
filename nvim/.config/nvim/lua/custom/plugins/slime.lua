428109b97ed6665e70d90a0bcf79f24d2ffd69d1
yiyuanh-x1carbon <yiyuanh@proton.me>
2024-09-09 21:30:51 -0700

    feat: change slime target to tmux

--- nvim/.config/nvim/lua/custom/plugins/slime.lua
+++ nvim/.config/nvim/lua/custom/plugins/slime.lua
@@ -1,7 +1,7 @@
 return {
   'jpalardy/vim-slime',
   init = function()
-    vim.cmd [[let g:slime_target = "wezterm"]]
-    vim.cmd [[let g:slime_default_config = {"pane_direction": "right"}]]
+    vim.cmd [[let g:slime_target = "tmux"]]
+    vim.cmd [[let g:slime_default_config = {"socket_name": get(split($TMUX, ","), 0), "target_pane": ":.1"}]]
   end,
 }
