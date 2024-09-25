8028fb15de5883f82a47c4cfcec180af8ac62e42
yiyuanh <yiyuanh@amazon.com>
2024-09-25 12:20:40 -0700

    chore: Change colorscheme to tokyo night

--- nvim/.config/nvim/lua/custom/plugins/everforest.lua
+++ nvim/.config/nvim/lua/custom/plugins/everforest.lua
@@ -3,8 +3,8 @@ return {
     'neanias/everforest-nvim',
     priority = 1000,
     init = function()
-      require('everforest').setup({})
-      require('everforest').load()
+      -- require('everforest').setup({})
+      -- require('everforest').load()
     end,
   }
 }
