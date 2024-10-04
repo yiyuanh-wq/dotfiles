f0354de305ef85c76cf3caf90de7a56eec5b8b5e
yiyuanh <yiyuanh@amazon.com>
2024-10-04 12:25:41 -0700

    feat: change colorscheme to github light

--- nvim/.config/nvim/lua/custom/plugins/material.lua
+++ nvim/.config/nvim/lua/custom/plugins/material.lua
@@ -3,8 +3,8 @@ return {
     'marko-cerovac/material.nvim',
     priority = 1000,
     init = function()
-      vim.g.material_style = "palenight"
-      vim.cmd.colorscheme 'material'
+      -- vim.g.material_style = "palenight"
+      -- vim.cmd.colorscheme 'material'
     end,
   }
 }
