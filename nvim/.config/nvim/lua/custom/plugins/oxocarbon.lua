75a2a8acfdd5b25ff8d1b2fe144ae9a38f8f008b
Michael He <yiyuanh@proton.me>
2024-10-07 19:51:02 -0700

    feat: switch colorscheme to palenight

--- nvim/.config/nvim/lua/custom/plugins/oxocarbon.lua
+++ nvim/.config/nvim/lua/custom/plugins/oxocarbon.lua
@@ -3,8 +3,8 @@ return {
     'nyoom-engineering/oxocarbon.nvim',
     priority = 1000,
     init = function()
-      vim.opt.background = "dark" -- set this to dark or light
-      vim.cmd.colorscheme "oxocarbon"
+      -- vim.opt.background = "dark" -- set this to dark or light
+      -- vim.cmd.colorscheme "oxocarbon"
     end,
   },
 }
