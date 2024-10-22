461e1160808a530955d2249e40c4e00e852291ef
yiyuanh <yiyuanh@amazon.com>
2024-10-22 09:47:24 -0700

    feat: add tomorrow night theme

--- nvim/.config/nvim/lua/custom/plugins/darcula.lua
+++ nvim/.config/nvim/lua/custom/plugins/darcula.lua
@@ -3,7 +3,7 @@ return {
     "xiantang/darcula-dark.nvim",
     priority = 1000,
     init = function()
-      vim.cmd.colorscheme 'darcula-dark'
+      -- vim.cmd.colorscheme 'darcula-dark'
     end,
   },
 }
---
+++ nvim/.config/nvim/lua/custom/plugins/tomorrow.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    'deparr/tairiki.nvim',
+    priority = 1000,
+    init = function()
+      require('tairiki').load()
+    end,
+  },
+}
