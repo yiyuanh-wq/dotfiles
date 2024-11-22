d3580c5bcab28727af5dcff6eb80573428ba2b95
Michael He <yiyuanh@proton.me>
2024-11-22 00:27:20 +0000

    change nvim colorscheme to catppuccin dark

--- nvim/.config/nvim/lua/custom/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/custom/plugins/catppuccin.lua
@@ -3,8 +3,17 @@ return {
     'catppuccin/nvim',
     name = 'catppuccin',
     priority = 1000,
+    opts = {
+      color_overrides = {
+        mocha = {
+          base = "#000000",
+          mantle = "#000000",
+          crust = "#000000",
+        },
+      },
+    },
     init = function()
-      -- vim.cmd.colorscheme 'catppuccin-mocha'
+      vim.cmd.colorscheme 'catppuccin-mocha'
     end,
   },
 }
