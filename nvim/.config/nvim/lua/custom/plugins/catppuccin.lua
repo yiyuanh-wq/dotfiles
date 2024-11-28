4f7c23577478140b69e713ee2ffc0a348a31d22b
Michael He <yiyuanh@proton.me>
2024-11-27 23:45:48 -0800

    change colors to edge light

--- nvim/.config/nvim/lua/custom/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/custom/plugins/catppuccin.lua
@@ -3,17 +3,8 @@ return {
     'catppuccin/nvim',
     name = 'catppuccin',
     priority = 1000,
-    opts = {
-      color_overrides = {
-        mocha = {
-          base = "#000000",
-          mantle = "#000000",
-          crust = "#000000",
-        },
-      },
-    },
     init = function()
-      vim.cmd.colorscheme 'catppuccin-mocha'
+      -- vim.cmd.colorscheme 'catppuccin-mocha'
     end,
   },
 }
---
+++ nvim/.config/nvim/lua/custom/plugins/edge.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    'sainnhe/edge',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- Optionally configure and load the colorscheme
+      -- directly inside the plugin declaration.
+      vim.g.edge_enable_italic = false
+      vim.g.edge_better_performance = 1
+      vim.cmd.colorscheme('edge')
+    end
+  }
+}
