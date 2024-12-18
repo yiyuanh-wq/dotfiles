b53ce7eaeb5527fc45c8fd4ed0bb537899f24f5b
yiyuanh <yiyuanh@amazon.com>
2024-12-18 11:40:53 -0800

    change colorscheme to gruvbox light hard

--- nvim/.config/nvim/lua/custom/plugins/nord.lua
+++ nvim/.config/nvim/lua/custom/plugins/nord.lua
@@ -4,16 +4,16 @@ return {
     lazy = false,
     priority = 1000,
     init = function()
-      require("nord").setup({
-        styles = {
-          comments = { italic = false },
-          bufferline = {
-            current = {},
-            modified = { italic = false },
-          }
-        }
-      })
-      vim.cmd.colorscheme("nord")
+      -- require("nord").setup({
+      --   styles = {
+      --     comments = { italic = false },
+      --     bufferline = {
+      --       current = {},
+      --       modified = { italic = false },
+      --     }
+      --   }
+      -- })
+      -- vim.cmd.colorscheme("nord")
     end,
   },
 }
--- nvim/.config/nvim/lua/kickstart/plugins/tokyonight.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/tokyonight.lua
@@ -14,6 +14,7 @@ return {
 
       -- You can configure highlights by doing something like:
       -- vim.cmd.hi 'Comment gui=none'
+      vim.cmd.colorscheme 'retrobox'
     end,
   },
 }
