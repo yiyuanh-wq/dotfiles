0849468d5a08cc43812d50e5a9e2ac9e16ea0650
yiyuanh <yiyuanh@proton.me>
2024-12-22 01:43:20 -0600

    set up new nvim config from scratch

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,40 @@
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
+vim.keymap.set("n", "<space>x", ":.lua<CR>")
+vim.keymap.set("v", "<space>x", ":lua<CR>")
+
+vim.opt.relativenumber = true
+vim.opt.background="dark"
+vim.opt.wrap = false
+vim.cmd("colorscheme retrobox")
+
+-- Use 4 spaces for each indentation
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.expandtab = true
+
+-- Highlight when yanking text
+vim.api.nvim_create_autocmd('TextYankPost', {
+    desc = 'Highlight when yanking text',
+    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+})
+
+-- Create the undodir if it doesn't exist
+local undodir = vim.fn.stdpath("data") .. "/undodir"
+-- Create directory if it doesn't exist
+if vim.fn.isdirectory(undodir) == 0 then
+    vim.fn.mkdir(undodir, "p")
+end
+
+-- Set undodir and enable persistent undo
+vim.opt.undodir = undodir
+vim.opt.undofile = true
+
+require("config.lazy")
