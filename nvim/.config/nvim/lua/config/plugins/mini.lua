05b4363d91ab7f4d03aeb03c6292991da2ef9ed9
yiyuanh <yiyuanh@proton.me>
2024-12-22 01:46:20 -0600

    add comment about enable/disable plugins

--- nvim/.config/nvim/lua/config/plugins/mini.lua
+++ nvim/.config/nvim/lua/config/plugins/mini.lua
@@ -1,6 +1,8 @@
 return {
     {
         'echasnovski/mini.nvim',
+        -- can set this for any plugin to easily turn on/off
+        -- enabled = false,
         config = function()
             local statusline = require 'mini.statusline'
             statusline.setup { use_icons = true }
