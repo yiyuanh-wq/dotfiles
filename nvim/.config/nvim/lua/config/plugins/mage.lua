23320a045f907fe74390fa8202adaf07422ef7a5
yiyuanh <yiyuanh@amazon.com>
2025-01-10 11:31:57 -0800

    modify neovim to use mage colorscheme

--- nvim/.config/nvim/lua/config/plugins/mage.lua
+++ nvim/.config/nvim/lua/config/plugins/mage.lua
@@ -1,10 +1,9 @@
 return {
-  "folke/tokyonight.nvim",
+  "yiyuan-he/mage.nvim",
   lazy = false, -- load immediately
   priority = 1000, -- make it's loaded before other colorschemes
-  enabled = false,
   opts = {},
   config = function ()
-    -- vim.cmd('colorscheme tokyonight-night')
+    vim.cmd('colorscheme mage')
   end
 }
