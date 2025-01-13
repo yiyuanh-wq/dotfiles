6c9ab657c08f4e020f00bcca34eade6869b01838
Michael He <yiyuanh@proton.me>
2025-01-12 19:08:03 -0800

    change colorscheme to tokyonight moon

--- nvim/.config/nvim/lua/config/plugins/mage.lua
+++ nvim/.config/nvim/lua/config/plugins/mage.lua
@@ -2,8 +2,9 @@ return {
   "yiyuan-he/mage.nvim",
   lazy = false, -- load immediately
   priority = 1000, -- make it's loaded before other colorschemes
+  enabled = false,
   opts = {},
   config = function ()
-    vim.cmd('colorscheme mage')
+    -- vim.cmd('colorscheme mage')
   end
 }
